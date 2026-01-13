4285dc85da2127f17521731b5faa48814923ca4e
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-01-13 16:07:21 +0530

    fixing

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,22 @@ const nextConfig = {
   // images: {
   //   domains: ["res.cloudinary.com", "heroui.com"],
   // },
-  remotePatterns: [
-    {
-      domain: "res.cloudinary.com",
-      // protocol: "https",
-      hostname: "res.cloudinary.com",
-      port: "",
-      // This allows all images from this host
-    },
-    {
-      protocol: "https",
-      hostname: "heroui.com",
-      port: "",
-      pathname: "/**",
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**", // This allows all images from this host
+      },
+      {
+        protocol: "https",
+        hostname: "heroui.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
