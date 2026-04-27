713275c9e4feda6b5578f04f50afb2cf512188ba
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-27 22:06:02 +0530

    feat: src/components/hero/Hero.tsx

--- src/components/hero/HeroServer.tsx
+++ src/components/hero/HeroServer.tsx
@@ -5,23 +5,22 @@ import Link from "next/link";
 const HeroSectionServer = () => {
   return (
     <div className="container mx-auto px-4 mt-15 text-center">
-      <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold mb-4">
+      <h1 className="mb-4 text-4xl font-bold sm:text-5xl md:text-6xl lg:text-7xl">
         Md Faizan Ahmad
       </h1>
-      <h6 className="text-2xl sm:text-2xl md:text-2xl lg:text-2xl font-bold mb-4">
-        I build fast, scalable web apps using React, Next.js & TypeScript
-      </h6>
-      {/* <p className="text-lg sm:text-xl md:text-2xl mb-6 text-[color:var(--muted-foreground)]">
-        Frontend Developer | Crafting Modern Web Experiences with AI & Design
-      </p> */}
-      <p className="text-lg sm:text-xl md:text-2xl mb-6 text-[color:var(--muted-foreground)]">
-        From UI to API integration — focused on performance, usability, and
-        clean code.
+
+      <h2 className="mb-4 text-2xl font-bold sm:text-2xl md:text-3xl">
+        Full Stack Developer | Frontend Developer
+      </h2>
+
+      <p className="mb-8 text-lg sm:text-xl md:text-2xl text-[color:var(--muted-foreground)]">
+        Building fast, responsive web apps with React, Next.js & TypeScript.
       </p>
-      <div className="flex flex-row flex-wrap justify-center gap-4">
+
+      <div className="flex flex-wrap justify-center gap-4">
         <Button
           asChild
-          className="bg-blue-800 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded-full transition-all duration-300 transform hover:scale-105"
+          className="bg-blue-800 hover:bg-blue-700 text-white font-semibold px-6 py-3 rounded-full transition-all duration-300 hover:scale-105"
         >
           <Link href="/contact">
             Contact <ArrowRight className="ml-2 h-5 w-5" />
@@ -31,17 +30,10 @@ const HeroSectionServer = () => {
         <Button
           asChild
           variant="outline"
-          className="border text-[color:var(--foreground)] hover:bg-[color:var(--card)]/80 font-semibold py-3 px-6 rounded-full transition-all duration-300 transform hover:scale-105"
+          className="border text-[color:var(--foreground)] hover:bg-[color:var(--card)]/80 font-semibold px-6 py-3 rounded-full transition-all duration-300 hover:scale-105"
         >
           <Link href="/projects">View Projects</Link>
         </Button>
-        {/* 
-        <Button
-          asChild
-          className="bg-green-600 hover:bg-green-700 text-white font-semibold py-3 px-6 rounded-full transition-all duration-300 transform hover:scale-105"
-        >
-          <Link href="/about">About</Link>
-        </Button> */}
       </div>
     </div>
   );
