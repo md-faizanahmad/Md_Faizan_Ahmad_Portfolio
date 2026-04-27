2dfe45775fecdea8284acdd97583e261bd0a7e7f
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-27 22:18:31 +0530

    feat: src/components/about/About.tsx

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "streak-stats.demolab.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
